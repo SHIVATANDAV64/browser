2105e1bac3fc614c76d0636a9163e307be3243da
Venkat Reddy <rudrashiva654@gmail.com>
2026-04-05 22:00:59 +0530

    feat: add support for ADVERTISE_HOST in start-lightpanda.sh

--- deploy/render/start-lightpanda.sh
+++ deploy/render/start-lightpanda.sh
@@ -3,6 +3,15 @@ set -eu
 
 PORT="${PORT:-10000}"
 LOG_LEVEL="${LIGHTPANDA_LOG_LEVEL:-info}"
+ADVERTISE_HOST="${LIGHTPANDA_ADVERTISE_HOST:-${RENDER_EXTERNAL_HOSTNAME:-}}"
+
+if [ -n "$ADVERTISE_HOST" ]; then
+  exec /usr/local/bin/lightpanda serve \
+    --host 0.0.0.0 \
+    --port "$PORT" \
+    --advertise-host "$ADVERTISE_HOST" \
+    --log-level "$LOG_LEVEL"
+fi
 
 exec /usr/local/bin/lightpanda serve \
   --host 0.0.0.0 \
